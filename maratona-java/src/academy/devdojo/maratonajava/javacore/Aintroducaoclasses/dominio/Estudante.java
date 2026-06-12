a0ceeec3805e6f4d0afb130117882c1ffd8545b3
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-06-12 11:17:54 -0300

    Aprendendo classes

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+
+public class Estudante {
+    public String nome = "Lucas";
+    public int idade = 18;
+    public char sexo = 'M';
+}
