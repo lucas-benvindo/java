c3efe98a164af24bc05dc0f51436b8de5378afe3
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-06-24 10:28:37 -0300

    Últimos ajustes do exercício

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Employee.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Employee.java
@@ -3,12 +3,12 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 public class Employee {
     public String name;
     public byte age;
-    public double[] salaries = new double[3];
+    public double[] salaries;
 
     public void dataPrinter() {
         System.out.println("Employee name: " + this.name);
         System.out.println("Employee age: " + this.age);
-        for (int index = 0; index < salaries.length; index++) {
+        for (int index = 0; index < salaries.length; index++) { //Prints the user's three salaries.
             System.out.printf("Employee salary %d: £%.2f%n", (index + 1), salaries[index]);
         }
     }
@@ -18,7 +18,7 @@ public class Employee {
         for (double salary : salaries) {
             total += salary;
         }
-        double average = total / salaries.length;
+        double average = total / salaries.length; //Average user salary
         System.out.printf("Average salary: £%.2f%n", average);
     }
 }
--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EmployeeTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EmployeeTest01.java
@@ -9,13 +9,15 @@ public class EmployeeTest01 {
     public static void main(String[] args) {
         Employee employee = new Employee();
         Scanner scanner = new Scanner(System.in).useLocale(Locale.UK);
+        employee.salaries = new double[3];
 
         System.out.println("======== DATA ENTRY ========");
         System.out.print("Full Name: ");
         employee.name = scanner.nextLine();
         System.out.print("Age: ");
         employee.age = scanner.nextByte();
-        for (int index = 0; index < employee.salaries.length; index++) {
+        scanner.nextLine(); //Clears the buffer
+        for (int index = 0; index < employee.salaries.length; index++) { //Scans the user's three pay stubs
             System.out.print("Salary " + (index + 1) + ": £");
             employee.salaries[index] = scanner.nextDouble();
         }
