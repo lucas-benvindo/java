b9a2223e662ef9d343695afd4a0219b637ae441d
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-06-24 13:30:36 -0300

    Modificadores de acesso, construtores e blocos de inicialização

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Eblocosdeinicializacao/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Eblocosdeinicializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Eblocosdeinicializacao.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+        }
+    }
+}
