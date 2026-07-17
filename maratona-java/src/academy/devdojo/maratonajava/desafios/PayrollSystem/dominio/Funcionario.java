042c0adabc9653cfa7286bf309e3e4e09b9c5214
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-17 11:56:40 -0300

    Constructor and getters implemented

--- maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/Funcionario.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/Funcionario.java
@@ -1,4 +1,19 @@
 package academy.devdojo.maratonajava.desafios.PayrollSystem.dominio;
 
 public class Funcionario {
+    private final String name;
+    private final double salary;
+
+    public Funcionario(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
