65e9d494cb0607e0e23d640f844fa1f67105d754
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-20 19:06:04 -0300

    Update Employee class

--- maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/Employee.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/Employee.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.desafios.PayrollSystem.dominio;
 
 public class Employee {
     private final String name;
+    private int hoursWorked;
 
     public Employee(String name) {
         this.name = name;
@@ -9,5 +10,13 @@ public class Employee {
 
     public String getName() {
         return name;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
     }
 }
