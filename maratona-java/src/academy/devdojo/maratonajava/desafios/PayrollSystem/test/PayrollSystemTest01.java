2b65a2073c16eb7101030c65af838cf958eb2e65
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-20 21:27:17 -0300

    Test completed

--- maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/test/PayrollSystemTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/test/PayrollSystemTest01.java
@@ -1,7 +1,29 @@
 package academy.devdojo.maratonajava.desafios.PayrollSystem.test;
 
+import academy.devdojo.maratonajava.desafios.PayrollSystem.dominio.CLT;
+import academy.devdojo.maratonajava.desafios.PayrollSystem.dominio.Company;
+import academy.devdojo.maratonajava.desafios.PayrollSystem.dominio.Intern;
+import academy.devdojo.maratonajava.desafios.PayrollSystem.dominio.PJ;
+
 public class PayrollSystemTest01 {
+
     public static void main(String[] args) {
+        Company apple = new Company("Apple");
+
+        CLT lucas = new CLT("Lucas", 1621, 10);
+        PJ agatha = new PJ("Agatha", 40, 20, 160);
+        Intern laura = new Intern("Laura", 1000);
+
+        apple.registerEmployee(lucas);
+        apple.registerEmployee(agatha);
+        apple.registerEmployee(laura);
+
+        apple.registerHours("Lucas", 240);
+        apple.registerHours("Agatha", 190);
+        apple.registerHours("Laura", 100);
+
+        apple.printAllPayslips();
+        apple.printTotalPayroll();
         
     }
 }
