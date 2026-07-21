ef6fad5ed669f798245f2f9bdbee7f92cb30bd1c
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-21 18:35:09 -0300

    School class constructor implemented

--- maratona-java/src/academy/devdojo/maratonajava/desafios/SchoolSystem/domain/School.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/SchoolSystem/domain/School.java
@@ -1,4 +1,11 @@
 package academy.devdojo.maratonajava.desafios.SchoolSystem.domain;
 
 public class School {
+    private final String name;
+    private final DayOfTheWeek dayOfTheWeek;
+
+    public School(String name, DayOfTheWeek dayOfTheWeek) {
+        this.name = name;
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
 }
