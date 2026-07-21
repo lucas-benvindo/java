5b8cc0693a2fb5013df4bc0b4da4402bcab367a5
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-21 19:11:57 -0300

    DayOfTheWeek constructor implemented

--- maratona-java/src/academy/devdojo/maratonajava/desafios/SchoolSystem/domain/DayOfTheWeek.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/SchoolSystem/domain/DayOfTheWeek.java
@@ -1,5 +1,27 @@
 package academy.devdojo.maratonajava.desafios.SchoolSystem.domain;
 
 public enum DayOfTheWeek {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    MONDAY(1, true),
+    TUESDAY(2, true),
+    WEDNESDAY(3, true),
+    THURSDAY(4, true),
+    FRIDAY(5, true),
+    SATURDAY(6, false),
+    SUNDAY(7, false);
+
+    private final int dayOfTheWeekNumber;
+    private final boolean isWeekday;
+
+    DayOfTheWeek(int dayOfTheWeekNumber, boolean isWeekday) {
+        this.dayOfTheWeekNumber = dayOfTheWeekNumber;
+        this.isWeekday = isWeekday;
+    }
+
+    public int getDayOfTheWeekNumber() {
+        return dayOfTheWeekNumber;
+    }
+
+    public boolean isWeekday() {
+        return isWeekday;
+    }
 }
