dc0d3f8397c6ec8472c095d967c76cdeaf866729
Lucas Benvindo <lucasbenvindo07cng@gmail.com>
2026-07-20 21:26:06 -0300

    Update PJ class

--- maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/PJ.java
+++ maratona-java/src/academy/devdojo/maratonajava/desafios/PayrollSystem/dominio/PJ.java
@@ -1,13 +1,18 @@
 package academy.devdojo.maratonajava.desafios.PayrollSystem.dominio;
 
+// Above hoursThreshold, a reduced rate kicks in (plateau effect).
 public class PJ extends Employee {
     private final double hourlyRate;
     private final double reducedRate;
+    private final double hoursThreshold;
 
-    public PJ(String name, double hourlyRate, double reducedRate) {
+    public PJ(String name, double hourlyRate, double reducedRate, double hoursThreshold) {
         super(name);
         this.hourlyRate = hourlyRate;
         this.reducedRate = reducedRate;
+        this.hoursThreshold = hoursThreshold;
+    }
+
     }
 
     public double getHourlyRate() {
@@ -16,5 +21,9 @@ public class PJ extends Employee {
 
     public double getReducedRate() {
         return reducedRate;
+    }
+
+    public double getHoursThreshold() {
+        return hoursThreshold;
     }
 }
